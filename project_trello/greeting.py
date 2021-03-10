968da21128230abbc453232cc3f7d1c6d3d28d95
siporter43 <67347341+siporter43@users.noreply.github.com>
2021-03-09 19:12:28 -0800

    moved this stuff to project_outrun

--- project_trello/greeting.py
+++
@@ -1,9 +0,0 @@
-def greeting(name):
-    """Return a welcome message string, including formatted name unless it's blank."""
-    name = name.strip().title()
-    if not name:
-        return "Welcome."
-    else:
-        return f"Welcome {name}."
-
-    
